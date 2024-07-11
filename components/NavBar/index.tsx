356372a1ba9401413c93269cab77087607382c94
Riyad139 <riyad.ahmed1886@gmail.com>
2024-07-12 03:35:36 +0600

    added

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -18,7 +18,7 @@ export default function NavBar() {
 
   return (
     <div className="w-full  fixed z-10 my-5 justify-center flex items-center bg  ">
-      <ul className="flex bg-gradient-to-tr from-gray-600 via-gray-700 to-gray-900 text-xs sm:text-sm font-semibold px-3 rounded-full max-w-fit bg-[#1A1A1A]  mx-[10%]  mb-[8%]   items-center">
+      <ul className="flex bg-gradient-to-tr from-gray-600 via-gray-700 to-gray-900 text-[10px] sm:text-sm font-semibold px-3 rounded-full max-w-fit bg-[#1A1A1A]  mx-[10%]  mb-[8%]   items-center">
         {NavItem.map((item) => (
           <li
             onClick={() => handleScroll(item.id)}
