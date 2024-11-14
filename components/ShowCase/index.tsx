c5f4bf3e2ca6c2d309b60597a569570bc75f13ca
Riyad139 <riyad.ahmed1886@gmail.com>
2024-11-14 19:31:05 +0600

    added

--- components/ShowCase/index.tsx
+++ components/ShowCase/index.tsx
@@ -22,31 +22,41 @@ export default function ShowCase() {
         Projects I worked on
       </p>
       <ProjectComponent
+        image="/projects/tour-camp.png"
+        title="Tour camp"
+        description="a travel blog platform with dynamic content structuring, secure payment processing using Stripe, and an interactive user interface built with Next.js and Tailwind CSS."
+        technology="Nextjs, Strapi.io , Stripe , tailwind , typescript , HTML , CSS"
+        gitUrl="https://github.com/shakilahmedriyad/Tour_camp"
+        demoUrl="https://tour-camp.vercel.app/"
+        reverse={false}
+      />
+      <ProjectComponent
         image={"/projects/NFT.jpg"}
         title="NFT Selling app"
         description="This app is designed and developed to Sell Nfts"
         technology="nextjs , tailwind"
         gitUrl="https://github.com/Riyad139/NFT-market"
         demoUrl="https://nft-market-demo-seven.vercel.app"
-        reverse={false}
+        reverse={true}
       />
       <ProjectComponent
         image="/projects/Management.jpg"
         title="Management app"
         description="A app which helps you to manage your task and projects"
         technology="nextjs, express , tailwind , typescript"
-        gitUrl="https://github.com/Riyad139/management"
-        demoUrl="https://management-woad.vercel.app"
-        reverse={true}
+        gitUrl="https://github.com/shakilahmedriyad/Tour_camp"
+        demoUrl="https://appmanagement.vercel.app/"
+        reverse={false}
       />
+
       <ProjectComponent
         image="/projects/Realtor.png"
         title="Realtor Landing page"
         description="This Landing page is design for a Realtor business"
         technology="HTML5 , SCSS"
-        demoUrl="https://riyad139.github.io/Realtor_app/"
-        gitUrl="https://github.com/Riyad139/Realtor_app"
-        reverse={false}
+        demoUrl="https://github.com/shakilahmedriyad/management"
+        gitUrl="https://github.com/shakilahmedriyad/Realtor_app"
+        reverse={true}
       />
     </div>
   );
